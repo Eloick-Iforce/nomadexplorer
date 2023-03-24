94d47d69d1eb462d54d12ab233ff0fad5eda23e7
Fratge <fratgepro@gmail.com>
2023-03-24 10:23:20 +0100

    feat: commentaire

--- src/Controller/StaticPages.php
+++ src/Controller/StaticPages.php
@@ -8,6 +8,10 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+use Symfony\Component\HttpFoundation\Request;
+
 class StaticPages extends AbstractController
 {
     #[Route('/', name: 'home')]
@@ -41,13 +45,72 @@ class StaticPages extends AbstractController
     }
 
     #[Route('/voyage/{id}', name: 'app_voyage_show', methods: ['GET'])]
-    public function show(Voyage $voyage): Response
-    {
+    public function show(Request $request, Voyage $voyage, VoyageRepository $voyageRepository): Response
+    {   
         $points = json_decode($voyage->getObject());
+
+        // Créer un nouveau commentaire
+        $commentaire = new Commentaire();
+
+        // Créer un formulaire pour le nouveau commentaire
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+
+        // Gérer la soumission du formulaire
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Ajouter le nouveau commentaire au voyage et le sauvegarder
+            $voyage->addCommentaire($commentaire);
+            $voyageRepository->save($voyage, true);
+
+            // Rediriger l'utilisateur vers la page de détails du voyage
+            return $this->redirectToRoute('app_voyage_show_admin', ['id' => $voyage->getId()]);
+        }
 
         return $this->render('voyage.html.twig', [
             'voyage' => $voyage,
             'points' => $points,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+    * @Route("/{id}/commentaire", name="app_voyage_commentaire", methods={"GET", "POST"})
+    */
+    public function ajouterCommentaire(Request $request, $id): Response
+    {
+        $voyage = $this->getDoctrine()->getRepository(Voyage::class)->find($id);
+
+        if (!$voyage) {
+            throw $this->createNotFoundException('Le voyage n\'existe pas');
+        }
+
+        $commentaire = new Commentaire();
+        $commentaire->setVoyage($voyage);
+
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire = $form->getData();
+
+            // Récupérer l'utilisateur actuel et le définir comme l'utilisateur du commentaire
+            $utilisateur = $this->getUser();
+            $commentaire->setUser($utilisateur);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre commentaire a été ajouté');
+
+            return $this->redirectToRoute('app_voyage_show', ['id' => $id]);
+        }
+
+        return $this->render('voyage/commentaire_ajouter.html.twig', [
+            'form' => $form->createView(),
+            'voyage' => $voyage,
         ]);
     }
 }
--- src/Controller/VoyageController.php
+++ src/Controller/VoyageController.php
@@ -11,6 +11,9 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+use App\Entity\Commentaire;
+use App\Form\CommentaireType;
+
 #[Route('/admin')]
 class VoyageController extends AbstractController
 {
@@ -96,5 +99,45 @@ class VoyageController extends AbstractController
         }
 
         return $this->redirectToRoute('app_voyage_admin', [], Response::HTTP_SEE_OTHER);
+    }
+
+    /**
+    * @Route("/{id}/commentaire", name="app_voyage_commentaire", methods={"GET", "POST"})
+    */
+    public function ajouterCommentaire(Request $request, $id): Response
+    {
+        $voyage = $this->getDoctrine()->getRepository(Voyage::class)->find($id);
+
+        if (!$voyage) {
+            throw $this->createNotFoundException('Le voyage n\'existe pas');
+        }
+
+        $commentaire = new Commentaire();
+        $commentaire->setVoyage($voyage);
+
+        $form = $this->createForm(CommentaireType::class, $commentaire);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $commentaire = $form->getData();
+
+            // Récupérer l'utilisateur actuel et le définir comme l'utilisateur du commentaire
+            $utilisateur = $this->getUser();
+            $commentaire->setUser($utilisateur);
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($commentaire);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Votre commentaire a été ajouté');
+
+            return $this->redirectToRoute('app_voyage_show', ['id' => $id]);
+        }
+
+        return $this->render('voyage/commentaire_ajouter.html.twig', [
+            'form' => $form->createView(),
+            'voyage' => $voyage,
+        ]);
     }
 }
